293051c8eaab3352ed3ce8c6f384082bae8a5931
ZRΣ <sreerajsr03@gmail.com>
2025-10-29 23:30:50 +0530

    Second smallest Number

---
+++ Arrays/7.findSecondSmallestNumber.js
@@ -0,0 +1,20 @@
+function findSecondSmallestNumber(arr){
+
+    if(arr.length <2){
+        return -1;
+    }
+    let smallest =Infinity;
+    let secondSmallest = Infinity;
+
+    for(let i=0;i<arr.length; i++){
+        if(arr[i]<smallest){
+            secondSmallest = smallest;
+            smallest= arr[i]
+        }else if(arr[i]< secondSmallest && arr[i]!==smallest){
+            secondSmallest=arr[i]
+        }
+    }
+    return secondSmallest;
+}
+
+console.log(findSecondSmallestNumber([1,2,-1,300,3]));

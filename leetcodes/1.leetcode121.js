de2b87e60bf966cf86a39cb3d4c876bc0663b746
ZRΣ <sreerajsr03@gmail.com>
2025-11-08 22:24:38 +0530

    solved leetcode problem

---
+++ leetcodes/1.leetcode121.js
@@ -0,0 +1,17 @@
+function getMaxProfit (prices){
+
+    let min =prices[0];
+    let profit =0;
+
+    for(let i=0; i<prices.length ; i++){
+        if(prices[i] - min > profit){
+            profit = prices[i] - min
+        }
+        if(prices[i] < min ){
+            min =prices[i]
+        }
+    }
+    return profit
+}
+
+console.log(getMaxProfit([7,1,5,3,6,4]))
